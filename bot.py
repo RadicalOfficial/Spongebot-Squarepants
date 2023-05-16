ed2ee9fae038805328ae34b928d2fdfa5f463981
speedwaysingapore <125277461+speedwaysingapore@users.noreply.github.com>
2023-05-16 09:39:16 -0400

    New - 2.1.0

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
-# THIS MAY BE OUTDATED. IF SO, PLEASE CONTACT @KNIGHTBOT63 ON SCRATCH
-from flask import Flask
-from threading import Thread
 import os
 import discord
 from discord import option
 import requests
 import time as t
 import random
+from flask import Flask
+from threading import Thread
+import logging
+from discord.ext.pages import Paginator, Page, PaginatorButton
+from discord.ext import commands
 
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 app = Flask('')
-
-print("Running")
 
 @app.route('/')
 def main():
@@ -22,69 +24,63 @@ def run():
 server = Thread(target=run)
 server.start()
 
+print("Loading... ")
+
 bot = discord.Bot()
+
+class Messenger(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.add_item(discord.ui.InputText(label="Long Input", style=discord.InputTextStyle.long))
+
+    async def callback(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Private Message:", description="Provided by Pycord Bot", color=discord.Color.random())
+        embed.add_field(name="Message:", value=self.children[0].value)
+        await interaction.response.send_message(embeds=[embed])
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 @bot.command(description="Sees if online")
 async def ping(ctx):
-  await ctx.respond(f"Pong! Latency is {bot.latency}")
-@bot.command(description="Does addition.")
-@option(
-    "first",
-    str,
-    description="Enter a number"
-)
-@option(
-    "second",
-    str,
-    description="Enter a number"
-)
-async def add(ctx, first: discord.Option(int), second: discord.Option(int)):
-  sum = first + second
-  await ctx.respond(f"The sum of {first} and {second} is {sum}.")
-#Pages for Leaderboard
+  await ctx.respond(f"Pong! Latency is {bot.latency}", ephemeral=True)
   
 @bot.command(description="Repeats after you")
-async def messenger(ctx, message: discord.Option(str), private: discord.Option(str)):
-  print(ctx.author, f"Message: {message} Private: {private}")
-  if private == "no":
-    author = str(ctx.author)
-  else:
-    author = "Anonymous User 👻"
-  print(author)
-  embed = discord.Embed(
-        title="Message Provider:",
-        description="Provided from your convienience.",
-        color=discord.Color.random(),
-    )
-  discord.Embed.set_author(embed, name=f"Written by: @{author}")
-  discord.Embed.add_field(embed, name="Message Content:", value=f"{message}")
-  embed.set_footer(text="---------\nThis is bad? Contact @k3\n\nProvided by Pycord")
-  await ctx.send(embed=embed)
+async def messenger(ctx: discord.ApplicationContext):
+  modal = Messenger(title="this is a title")
+  await ctx.send_modal(modal)
+  await ctx.respond("Success! The message you have sent is now available to the public!", ephemeral=True)
 
-@bot.command(description="Get Help via Direct Messages (Filling out the help box can be anything)")
-async def help(ctx, member: discord.Member):
-  print(member)
-  member = ctx.author
-  await ctx.respond("Sent a Direct Message to you, check your inbox for help!", ephemeral=True)
-  embed = discord.Embed(
-        title="Commands",
-        description="Here are the list of commands, most of these are updated to the current version.",
-        color=discord.Colour.blurple())
-  embed.add_field(name="/help", value="Get's you here", inline=False)
-  embed.add_field(name="/ping", value="Check if bot is online", inline=False)
-  embed.add_field(name="/messenger", value="Sends secret messages if selected 'no'", inline=False)
-  embed.set_author(name="Creator of Bot: k3")
-  embed.set_footer(text="Update version: v2.0.2")
-  await member.send(embed=embed)
-
+@bot.command(description="Get Help via the tables.")
+async def help(ctx):
+  my_pages = [
+    Page(
+        embeds=[
+            discord.Embed(title="Basic Commands:", description="/help - Direct Messages for help\n/messenger - Sends a private message in the area.\n/ping - Checks if the bot is alive\n"),
+        ],
+    ),
+    Page(embeds=[
+            discord.Embed(title="Advanced Commands:", description="/check_scratchdb - Checks if ScratchDB is up or not\n")],),
+    Page(embeds=[
+            discord.Embed(title="Questions? Feedback?", description="Direct Message @k3!\n\n**Other:**\nCurernt Version: ```2.0.3```\n*Credits:*\nPyCord for code\nEverything else by @k3 (creator of this bot)")],),]
+  buttons = [
+            PaginatorButton("first", label="⏮", style=discord.ButtonStyle.blurple),
+            PaginatorButton("prev", label="⏪", style=discord.ButtonStyle.blurple),
+            PaginatorButton("page_indicator", style=discord.ButtonStyle.gray, disabled=True),
+            PaginatorButton("next", label="⏩", style=discord.ButtonStyle.blurple),
+            PaginatorButton("last", label="⏭", style=discord.ButtonStyle.blurple),
+        ]
+  paginator = Paginator(
+            pages=my_pages,
+            show_indicator=True,
+            use_default_buttons=False,
+            custom_buttons=buttons,
+        )
+  await paginator.respond(ctx.interaction)
 @bot.command(description="Checks if ScratchDB is Up or Not.")
 async def check_scratchdb(ctx):
-  await ctx.respond("Request sent!", ephemeral=True)
   data = requests.get("https://scratchdb-checker.knightbot63.repl.co").json()['Status']
-  await ctx.respond(data)
+  await ctx.respond("ScratchDB is Currently: " + data, ephemeral=True)
 @bot.command(description="Fun game to check if you are human")
 async def vibe_check(ctx):
   await ctx.respond("**Loading your content!**", ephemeral=True)
@@ -93,5 +89,17 @@ async def vibe_check(ctx):
   if random.randint(1,2) == 1:
     await msg.edit(content="*You're good.*")
   else:
-    await msg.edit(content="You're not good. You are a **demon**.")
+    await msg.edit(content="You're not good.")
+@bot.command(description="Bot Updates or other announcements")
+async def announcement(ctx, title: discord.Option(str), message: discord.Option(str), announcement: discord.Option(str), ping_everyone: discord.Option(str)):
+  if str(ctx.author) == "k3#8331":
+    embed = discord.Embed(title=title, description=message)
+    embed.add_field(name="", value=announcement, inline=True)
+    embed.set_author(name="Pycord Bot Team", icon_url="https://cdn.discordapp.com/avatars/1095344733104128173/8267b033dec75ddc733b35debc4cc4ad.webp?size=80")
+    await ctx.respond(embed=embed)
+  else:
+    await ctx.respond("What do you think you are doing?", ephemeral=True)
+@bot.command(description="Meme Generator")
+async def meme_generator(ctx):
+  pass
 bot.run(os.environ['token'])
